2b85da49c4a0d5099b9496ba157927aca7cdf1db
tazkiajessy <jessy566eee@gmail.com>
2021-03-22 13:08:14 +0100

    spcae table migrate, model and seeded

---
+++ seeders/20210322115510-some-spaces.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "spaces",
+      [
+        {
+          title: "How i become a programmer",
+          description: "How i learned to stop worrying and love the typos",
+          backgroundColor: "#ffffff",
+          color: "#000000",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "How i become an artist",
+          description:
+            "How i learned to inspire from nature and implemented so",
+          backgroundColor: "#ffffff",
+          color: "#000000",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("spaces", null, {});
+  },
+};
