88bd506b0cacc375c1d162d1159b42d347f26688
tazkiajessy <jessy566eee@gmail.com>
2021-03-22 14:53:38 +0100

    signup new space created

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Space = require("../models").space;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -21,7 +22,7 @@ router.post("/login", async (req, res, next) => {
 
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(400).send({
-        message: "User with that email not found or password incorrect"
+        message: "User with that email not found or password incorrect",
       });
     }
 
@@ -44,14 +45,25 @@ router.post("/signup", async (req, res) => {
     const newUser = await User.create({
       email,
       password: bcrypt.hashSync(password, SALT_ROUNDS),
-      name
+      name,
     });
+
+    const newSpace = await Space.create({
+      title: "default title",
+      userId: newUser.id,
+      backgroundColor: "#ffffff",
+      color: "#000000",
+    });
+
+    console.log("newspace", newSpace);
 
     delete newUser.dataValues["password"]; // don't send back the password hash
 
     const token = toJWT({ userId: newUser.id });
 
-    res.status(201).json({ token, ...newUser.dataValues });
+    res
+      .status(201)
+      .json({ token, ...newUser.dataValues, ...newSpace.dataValues });
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
       return res
